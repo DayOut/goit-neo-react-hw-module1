e5a4521bf27c5e8ed925d139a219ef63cc03c4c2
Dayout <fearess7@gmail.com>
2025-02-08 15:10:16 +0200

    Move FriendListItem to separate folder to fit all requirements. Code style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import userData from "./data/userData.json";
 import friendListData from "./data/friendListData.json";
 import transactionHistoryData from "./data/transactionHistoryData.json";
@@ -8,18 +8,18 @@ import TransactionHistory from "./components/TransactionHistory/TransactionHisto
 
 function App() {
   return (
-      <div className="App">
-          <Profile
-              name={userData.username}
-              tag={userData.tag}
-              location={userData.location}
-              image={userData.avatar}
-              stats={userData.stats}
-          />
-          <FriendList friends={friendListData} />
-          <TransactionHistory items={transactionHistoryData} />
-      </div>
+    <div className="App">
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friendListData} />
+      <TransactionHistory items={transactionHistoryData} />
+    </div>
   );
 }
 
-export default App
+export default App;
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,37 +2,37 @@ import PropTypes from "prop-types";
 import style from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
-    return (
-        <table className={style.transactionHistory}>
-            <thead>
-            <tr>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Currency</th>
-            </tr>
-            </thead>
-            <tbody>
-            {items.map(({ id, type, amount, currency }) => (
-                <tr key={id}>
-                    <td>{type}</td>
-                    <td>{amount}</td>
-                    <td>{currency}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
-    );
+  return (
+    <table className={style.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            amount: PropTypes.string.isRequired,
-            currency: PropTypes.string.isRequired,
-        })
-    ).isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default TransactionHistory;
